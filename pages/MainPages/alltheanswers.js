913debb19e784df1cbe6403eabf52c1149bc25c6
seonjin Ahn <seonjin0611@gmail.com>
2021-08-27 19:48:03 +0900

    모두의 답변 보기도 인기이별록처럼

--- pages/MainPages/alltheanswers.js
+++ pages/MainPages/alltheanswers.js
@@ -4,7 +4,9 @@ import { StatusBar } from 'expo-status-bar';
 import firebase from 'firebase/app';
 import { firebase_db } from '../../firebaseConfig';
 import Icon from 'react-native-vector-icons/AntDesign';
-
+import { useHeaderHeight } from '@react-navigation/stack';
+import { getBottomSpace } from 'react-native-iphone-x-helper'
+import { getStatusBarHeight } from 'react-native-status-bar-height';
 const test1 = {
     questions:""
   }
@@ -44,36 +46,121 @@ const alltheanswers = ({ navigation, route }) => {
     console.log("allthequestions question",questions)
     test1.questions=questions
 
-    const [answers, setAnswers] = useState([]);
-    useEffect(()=>{
-        firebase_db.ref(`questions/${colorAnswers}/${questionsKey}/`)
-        .on('value', (snapshot)=>{
-            let temp = [];
-            snapshot.forEach((child)=>{
-                const item={
-                    ...child.val(),
-                    key: child.key
-                }
-                temp.push(item)
-                console.log("item: " + item)
-            })
-            temp.sort(function (a,b){
-                return new Date(b.regdate) - new Date(a.regdate);
-            })
-            setAnswers(temp)
-        })
-    },[])
 
-    console.log("answers",answers)
+    // const [answers, setAnswers] = useState([]);
+    // useEffect(()=>{
+    //     firebase_db.ref(`questions/${colorAnswers}/${questionsKey}/`)
+    //     .on('value', (snapshot)=>{
+    //         let temp = [];
+    //         snapshot.forEach((child)=>{
+    //             const item={
+    //                 ...child.val(),
+    //                 key: child.key
+    //             }
+    //             temp.push(item)
+    //             console.log("item: " + item)
+    //         })
+    //         temp.sort(function (a,b){
+    //             return new Date(b.regdate) - new Date(a.regdate);
+    //         })
+    //         setAnswers(temp)
+    //     })
+    // },[])
+
+    // console.log("answers",answers)
     
+
+    const [list, setList] = useState([]);
+    const [hotcolor, setHotColor] = useState("#21381c")
+    const [newcolor, setNewColor] = useState("#E9E9E9")
+    const headerHeight = useHeaderHeight();
+    const ScreenHeight = Dimensions.get('window').height   //height
+    const BottomSpace = getBottomSpace()
+    const tabBarHeight = 0
+    const statusBarHeight = getStatusBarHeight();
+    const realScreen = ScreenHeight - headerHeight - BottomSpace - tabBarHeight;
+
+
+    useEffect(() => {
+        firebase_db
+                .ref(`book`)
+                .on('value', (snapshot) => {
+                        let list = [];
+                        let temp = [];
+                        snapshot.forEach((child) => {
+                                const book = child.val();
+                                const { both } = book;
+                                //      console.log("useeffectbook",book)
+
+                                if (both == undefined) {
+                                        console.log("PopularArticle() 챕터가 없습니다")
+                                } else {
+                                        list = [...list, ...Object.values(both)]; // spread를 통한 리스트 병합
+                                }
+                                const arraylist = Object.values(list)
+                                const listFiltered = arraylist.filter(filteredList => filteredList.isPublic == true && filteredList.questionsKey == questionsKey )
+
+                                listFiltered.sort(function (a, b) {
+                                        return (b.likeCount) - (a.likeCount)
+                                })
+                                setList(listFiltered);
+                        })
+                })
+}, []) // 여기에 원래 list 가 있었음... 이거 없애니 렉은 안걸림
+// console.log("populararticlelist",list)
+
+
+    const viewHot = () => {
+            // console.log("viewHot")
+            const hotlist = [...list];
+            hotlist.sort(function (a, b) {
+                    return (b.likeCount) - (a.likeCount)
+            })
+            setList(hotlist);
+            // console.log("viewHot done")
+            // console.log ("list 2 (hot): " + {list});
+
+            setHotColor("#21381C");
+            setNewColor("#E9E9E9")
+    }
+
+    const viewNew = () => {
+            // console.log("viewNew")
+            const newlist = [...list]
+            newlist.sort(function (a, b) {
+                    return new Date(b.regdate) - new Date(a.regdate);
+            })
+            setList(newlist);
+            // console.log("viewNew done")
+            // console.log("list 3 (new): " + {list});
+
+            setHotColor("#E9E9E9")
+            setNewColor("#21381C")
+    }
+    console.log("liestalltheanswers",list)
+
     return (
         <SafeAreaView style={{flex:1}}>
+                                    <View style={{ flexDirection: "row", height: "3%", marginVertical: "2%" }}>
+                                <TouchableOpacity
+                                        style={{ flex: 1, marginLeft: "2%", marginRight: "1%" }}
+                                        onPress={() => viewHot()}>
+                                        <Text style={{ alignSelf: "center", fontSize: 17 }} > 인기 답변 </Text>
+                                        <View style={{ fontSize: 17, borderBottomWidth: 3, borderBottomColor: hotcolor, marginTop: "3%" }} />
+                                </TouchableOpacity>
+                                <TouchableOpacity
+                                        style={{ flex: 1, marginRight: "2%", marginRight: "1%" }}
+                                        onPress={() => viewNew()}>
+                                        <Text style={{ fontize: 17, alignSelf: "center", fontSize: 17 }}> 최신 답변 </Text>
+                                        <View style={{ fontSize: 17, borderBottomWidth: 3, borderBottomColor: newcolor, marginTop: "3%" }} />
+                                </TouchableOpacity>
+                        </View>
                 <Text style={{alignSelf:"center", fontSize:"20", marginTop:"2%"}}>{questions.title}</Text>
                 <Text style={{alignSelf:"center", fontSize:"15", marginTop:"2%", marginHorizontal:"10%"}}>{questions.intro}</Text>
 
                 <ScrollView style={{height:500}}>
 
-                        {answers.map(item => {
+                {list.map(item  => {
                             return (
                                 <PostItem
                                     navigation={navigation}
